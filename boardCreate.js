b42c903135bf2ed23f58c8767df4a3c84847967d
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-10-29 20:14:13 +1300

    Edit [boardCreate.js, knight.js].

--- boardCreate.js
+++ boardCreate.js
@@ -1,8 +1,18 @@
 function boardCreate () {
-    const board_column = [0, 1, 2, 3, 4, 5, 7];
-    const board_row = [0, 1, 2, 3, 4, 5, 7];
+    const board_coordinate = [0, 1, 2, 3, 4, 5, 6, 7];
+    const board = [];
 
-    console.log(board_column);   
+    board_coordinate.forEach((column) => {
+        const coordinate = [];
+        board_coordinate.forEach((row) => {
+            const XY = [column , row];
+            coordinate.push(XY);
+        })
+
+        board.push(coordinate);
+    });
+
+    return board;
 }
 
 export {boardCreate};
--- knight.js
+++ knight.js
@@ -1,7 +1,32 @@
 import {boardCreate} from './boardCreate.js';
 
-function knightMove () {
+const board = boardCreate()
 
+const moveFactory = (column, row) => {
+    let current_position = {column, row};
+    // +2C, -R, +R
+    let up_left_right = {up: (column + 2), left: (row - 1), right: (row + 1)};
+    // -2C, -R, +R
+    let down_left_right = {down: (column + 2), left: (row - 1), right: (row + 1)};
+    // -2R, +C, -C]
+    let left_up_down = {left: (column - 2), up: (row + 1), down: (row - 1)};
+    // +2R, +C, -C
+    let right_up_down = {right: (column + 2), up: (row + 1), down: (row - 1)}; 
+
+    
+    const getUpLeftRight = () => {return up_left_right};
+    const getDownLeftRight = () => {return down_left_right};
+    const getLeftUpDown = () => {return left_up_down};
+    const getRightUpDown = () => {return right_up_down};
+
+    return {getUpLeftRight, getDownLeftRight, getLeftUpDown, getRightUpDown};
+}   
+
+function knightMove (column, row) {
+    const moves = moveFactory(column, row);
+    console.log(moves.getUpLeftRight());
+    console.log(moves.getDownLeftRight());
 }
 
-boardCreate();
+knightMove(0, 0);
+console.log(board[7][7]);
