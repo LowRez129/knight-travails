b9fa999ad4b2115388400befaec799b0bae9b9f9
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-10-31 15:54:07 +1300

    Edit knight.js and knightFactory for refactoring.

--- knight.js
+++ knight.js
@@ -1,14 +1,12 @@
 import {boardCreate} from './boardCreate.js';
 import {knightFactory} from './knightFactory.js';
 
-function knightMoves (start = [0, 0], end = [0, 0]) {
+function knightMoves (start = [0, 0], end = [7, 7]) {
     const board = boardCreate()
-    const knight = knightFactory(start[0], start[1]);
+    const knight = knightFactory(start);
 
     knight.printAttributes();
-    //knight.setPosition(knight.getUpLeftRight()[0], knight.getUpLeftRight()[1][1]);
-    //knight.printAttributes();
-    //console.log(board[7][7]);
+    knight.setPosition();
 }
 
-knightMoves([0, 2], [3, 5]);
+knightMoves();
--- knightFactory.js
+++ knightFactory.js
@@ -1,11 +1,15 @@
-function knightFactory (column, row) {
-    let current_position;
-    let up_left_right;
-    let down_left_right;
-    let left_up_down;
-    let right_up_down;
+function knightFactory (position = [0, 0]) {
+    let current_position = null;
+    let up_left_right = null;
+    let down_left_right = null;
+    let left_up_down = null;
+    let right_up_down = null;
 
-    const setPosition = (c, r) => {
+    const setPosition = (array) => {
+        if (array == null) {return console.log("Invalid Input")};
+
+        const c = array[0];
+        const r = array[1];
         const up = c + 2;
         const down = c - 2;
         const left = r - 2;
@@ -16,19 +20,35 @@ function knightFactory (column, row) {
         const y_down = ((c - 1) < 0) ? null : c - 1;
 
         current_position = [c, r];
-        up_left_right = [up, [x_left, x_right]];        //{up: (c + 2), left: (r - 1), right: (r + 1)};
-        down_left_right = [down, [x_left, x_right]];    //{down: (c - 2), left: (r - 1), right: (r + 1)};
-        left_up_down = [[y_up, y_down], left];          //{left: (c - 2), up: (r + 1), down: (r - 1)};
-        right_up_down = [[y_up, y_down], right];        //{right: (c + 2), up: (r + 1), down: (r - 1)}; 
-        
+        up_left_right = [[up, x_left], [up, x_right]];        //{up: (c + 2), left: (r - 1), right: (r + 1)};
+        down_left_right = [[down, x_left], [down, x_right]];    //{down: (c - 2), left: (r - 1), right: (r + 1)};
+        left_up_down = [[y_up, left], [y_down, left]];          //{left: (c - 2), up: (r + 1), down: (r - 1)};
+        right_up_down = [[y_up, right], [y_down, right]];        //{right: (c + 2), up: (r + 1), down: (r - 1)}; 
         
         if (up > 7) {up_left_right = null};
         if (down < 0) {down_left_right = null};
         if (left < 0) {left_up_down = null};
         if (right > 7) {right_up_down = null};
+        
+        const moves = [up_left_right, down_left_right, left_up_down, right_up_down];
+        moves.forEach((child) => {
+            if (child == null) {return};
+            child.forEach((element) => {
+                element.forEach((index) => {
+                    if (index == null) {
+                        const num_index = child.indexOf(element)
+                        child[num_index] = null;
+                        //console.log();
+                        console.log(element);
+                        return;
+                    }
+                });
+            });
+        });
+        
     } 
 
-    setPosition(column, row);
+    setPosition(position);
 
     const getCurrentPosition = () => {return current_position};
     const getUpLeftRight = () => {return up_left_right};
