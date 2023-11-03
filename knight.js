faa361d5247b1e8a3c4bb4a4b2453d0446bf6b02
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-11-03 16:23:36 +1300

    Edit [knight.js, knightFactory.js].

--- knight.js
+++ knight.js
@@ -1,19 +1,71 @@
 //import {boardCreate} from './boardCreate.js';
 import {knightFactory} from './knightFactory.js';
 
-function moveTree(value = [0, 0]) {
-    if (value == null) {return};
-    const knight = knightFactory(value);
-    const node_UL = moveTree(knight.getUpLeft());
+function knightMove (start, end) {
+    if (start.toString() == [null].toString()) {return};
+    const knight = knightFactory(start);
+    const end_string = end.toString();
+    const path = [];
+    const move_queue = [];
 
-    const getKnight = () => {return knight};
-    const getNode = () => {return node_UL};
+    while (true) {
+        
+        if (knight.getCurrentPosition().toString() != [null]) {
+            path.push(knight.getCurrentPosition());
+        };
 
-    return {getKnight, getNode};
+        move_queue.push(
+            knight.getUpLeft(),
+            knight.getUpRight(),
+            knight.getDownLeft(),
+            knight.getDownRight(),
+            knight.getLeftUp(),
+            knight.getLeftDown(),
+            knight.getRightUp(),
+            knight.getRightDown()
+        );   
+
+        if (end_string == knight.getUpLeft()) {
+            path.push(knight.getUpLeft());
+            return console.log(path)
+        };
+        if (end_string == knight.getUpRight()) {
+            path.push(knight.getUpRight());
+            return console.log(path)
+        };
+        if (end_string == knight.getDownLeft()) {
+            path.push(knight.getDownLeft())
+            return console.log(path)
+        };
+        if (end_string == knight.getDownRight()) {
+            path.push(knight.getDownRight())
+            return console.log(path)
+        };
+        if (end_string == knight.getLeftUp()) {
+            path.push(knight.getLeftUp());
+            return console.log(path)
+        };
+        if (end_string == knight.getLeftDown()) {
+            path.push(knight.getLeftDown())
+            return console.log(path)
+        };
+        if (end_string == knight.getRightUp()) {
+            path.push(knight.getRightUp());
+            return console.log(path)
+        };
+        if (end_string == knight.getRightDown()) {
+            path.push(knight.getRightDown());
+            return console.log(path)
+        };
+        
+        const move_queue_pop = move_queue.shift();
+        
+        if (move_queue_pop.toString() != [null].toString()) {
+            knight.setPosition(move_queue_pop);
+        };
+    }
 }
-const TREE = moveTree([3, 3]);
-TREE.getKnight().printAttributes();
-console.log();
-TREE.getNode().getKnight().printAttributes();
-console.log();
-TREE.getNode().getNode().getKnight().printAttributes();
+
+knightMove([0, 0], [3, 3]);
+
+
--- knightFactory.js
+++ knightFactory.js
@@ -1,18 +1,18 @@
-function knightFactory (position = null) {
-    if (position == null) {return null};
+function knightFactory (position = [null]) {
+    if (position == [null]) {return [null]};
 
-    let current_position = null;
-    let up_left = null;
-    let up_right = null;
-    let down_left = null;
-    let down_right = null;
-    let left_up = null;
-    let left_down = null;
-    let right_up = null;
-    let right_down = null;
+    let current_position = [null];
+    let up_left = [null];
+    let up_right = [null];
+    let down_left = [null];
+    let down_right = [null];
+    let left_up = [null];
+    let left_down = [null];
+    let right_up = [null];
+    let right_down = [null];
 
     const setPosition = (array = [0, 0]) => {
-        if (array == null) {return console.log("Invalid Input")};
+        if (array == [null]) {return console.log("Invalid Input")};
 
         const c = array[0];
         const r = array[1];
@@ -20,25 +20,26 @@ function knightFactory (position = null) {
         const down = c - 2;
         const left = r - 2;
         const right = r + 2;
-        const x_left = ((r - 1) < 0) ? null : r - 1;
-        const x_right = ((r + 1) > 7) ? null : r + 1;
-        const y_up = ((c + 1) > 7) ? null : c + 1;
-        const y_down = ((c - 1) < 0) ? null : c - 1;
+        const x_left = ((r - 1) < 0) ? [null] : r - 1;
+        const x_right = ((r + 1) > 7) ? [null] : r + 1;
+        const y_up = ((c + 1) > 7) ? [null] : c + 1;
+        const y_down = ((c - 1) < 0) ? [null] : c - 1;
+        const null_string = [null].toString();
 
         current_position = [c, r];
-        up_left = (x_left == null) ? null : [up, x_left]; 
-        up_right = (x_right == null) ? null : [up, x_right];
-        down_left = (x_left == null) ? null : [down, x_left];
-        down_right = (x_right == null) ? null : [down, x_right];
-        left_up = (y_up == null) ? null : [y_up, left];
-        left_down = (y_down == null) ? null : [y_down, left];        
-        right_up = (y_up == null) ? null : [y_up, right];    
-        right_down = (y_down == null) ? null : [y_down, right];
+        up_left = (x_left.toString() == null_string) ? [null] : [up, x_left]; 
+        up_right = (x_right.toString() == null_string) ? [null] : [up, x_right];
+        down_left = (x_left.toString() == null_string) ? [null] : [down, x_left];
+        down_right = (x_right.toString() == null_string) ? [null] : [down, x_right];
+        left_up = (y_up.toString() == null_string) ? [null] : [y_up, left];
+        left_down = (y_down.toString() == null_string) ? [null] : [y_down, left];        
+        right_up = (y_up.toString() == null_string) ? [null] : [y_up, right];    
+        right_down = (y_down.toString() == null_string) ? [null] : [y_down, right];
 
-        if (up > 7) {up_left = null; up_right = null};
-        if (down < 0) {down_left = null; down_right = null;};
-        if (left < 0) {left_up = null; left_down = null;};
-        if (right > 7) {right_up = null; right_down = null;};
+        if (up > 7) {up_left = [null]; up_right = [null]};
+        if (down < 0) {down_left = [null]; down_right = [null];};
+        if (left < 0) {left_up = [null]; left_down = [null];};
+        if (right > 7) {right_up = [null]; right_down = [null];};
     } 
 
     setPosition(position);
