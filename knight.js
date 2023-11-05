fe65d2c52a083a5a99a9dfba6ebdf509cbab68c5
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-11-06 12:32:44 +1300

    Edit knight.js

--- knight.js
+++ knight.js
@@ -9,43 +9,24 @@ function knightMove (start, end) {
     const move_queue = [];
 
     while (true) {
+        const move_check = [];
+        move_check.push(
+            knight.getUpLeft(), knight.getUpRight(), knight.getDownLeft(), knight.getDownRight(),
+            knight.getLeftUp(), knight.getLeftDown(), knight.getRightUp(), knight.getRightDown()
+        );
+
+        for (let i = 0; i < 7; i++) {
+            if (move_check[i] == end.toString()) {
+                path.push(move_check[i]);
+                console.log(path);
+                return;
+            }
+        }
+
         move_queue.push(
             knight.getUpLeft(), knight.getUpRight(), knight.getDownLeft(), knight.getDownRight(),
             knight.getLeftUp(), knight.getLeftDown(), knight.getRightUp(), knight.getRightDown()
-        );   
-
-        if (end_string == knight.getUpLeft()) {
-            path.push(knight.getUpLeft());
-            return console.log(path)
-        };
-        if (end_string == knight.getUpRight()) {
-            path.push(knight.getUpRight());
-            return console.log(path)
-        };
-        if (end_string == knight.getDownLeft()) {
-            path.push(knight.getDownLeft())
-            return console.log(path)
-        };
-        if (end_string == knight.getDownRight()) {
-            path.push(knight.getDownRight())
-            return console.log(path)
-        };
-        if (end_string == knight.getLeftUp()) {
-            path.push(knight.getLeftUp());
-            return console.log(path)
-        };
-        if (end_string == knight.getLeftDown()) {
-            path.push(knight.getLeftDown())
-            return console.log(path)
-        };
-        if (end_string == knight.getRightUp()) {
-            path.push(knight.getRightUp());
-            return console.log(path)
-        };
-        if (end_string == knight.getRightDown()) {
-            path.push(knight.getRightDown());
-            return console.log(path)
-        };
+        );
         
         const move_queue_pop = move_queue.shift();
         
